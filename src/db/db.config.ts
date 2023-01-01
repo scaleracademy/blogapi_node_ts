3fc3da5ffc625a8c4b3e18b3f92c725303b3e669
Arnav Gupta <championswimmer@gmail.com>
2023-01-01 15:05:11 +0530

    orm configurations complete
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,9 +1,11 @@
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { defineConfig } from '@mikro-orm/sqlite';
 
 export default defineConfig({
   dbName: 'blog.db.sqlite',
   entitiesTs: ['src/**/*.entity.ts'],
   entities: ['dist/**/*.entity.js'],
+  metadataProvider: TsMorphMetadataProvider,
   debug: true, // TODO: disable in production
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { initORM } from './db/orm.init';
+import { RequestContext } from '@mikro-orm/core';
 
 const app = express();
 
@@ -8,7 +9,11 @@ app.get('/', (req, res) => {
 })
 
 async function start() {
-  await initORM();
+  const orm = await initORM();
+
+  app.use((req, res, next) => {
+    RequestContext.create(orm.em, next);
+  })
 
   app.listen(8181, () => {
     console.log('Server is running on http://localhost:8181')
